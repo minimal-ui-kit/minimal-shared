f5ce0c6376c47a034f0277959b497b654099be51
minimal-ui-kit <minimal.ui.kit@gmail.com>
2024-11-18 23:10:30 +0700

    Update eslint

--- packages/internal-eslint-config/typescript.mjs
+++ packages/internal-eslint-config/typescript.mjs
@@ -10,143 +10,142 @@ import unusedImportsPlugin from 'eslint-plugin-unused-imports';
 // ----------------------------------------------------------------------
 
 /**
- * Custom ESLint rules configuration.
+ * @rules common
+ * from 'react', 'eslint-plugin-react-hooks'...
  */
-const customRules = {
-  /**
-   * Common rules from 'react', 'typescript' and 'eslint-plugin-react-hooks'.
-   */
-  common: {
-    ...reactHooksPlugin.configs.recommended.rules,
-    'func-names': 1,
-    'no-bitwise': 2,
-    'object-shorthand': 1,
-    'no-useless-rename': 1,
-    'default-case-last': 2,
-    'consistent-return': 2,
-    'no-constant-condition': 1,
-    'default-case': [2, { commentPattern: '^no default$' }],
-    'lines-around-directive': [2, { before: 'always', after: 'always' }],
-    'arrow-body-style': ['error', 'as-needed', { requireReturnForObjectLiteral: false }],
-    // react
-    'react/jsx-key': 0,
-    'react/display-name': 0,
-    'react/no-children-prop': 0,
-    'react/jsx-boolean-value': 2,
-    'react/self-closing-comp': 2,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-no-useless-fragment': [1, { allowExpressions: true }],
-    'react/jsx-curly-brace-presence': [2, { props: 'never', children: 'never' }],
-    // typescript
-    '@typescript-eslint/no-shadow': 2,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-empty-object-type': 0,
-    '@typescript-eslint/consistent-type-imports': 1,
-    '@typescript-eslint/no-unused-vars': [1, { args: 'none' }],
-  },
-  /**
-   * Import rules from 'eslint-plugin-import'.
-   */
-  importPlugin: {
-    ...importPlugin.configs.recommended.rules,
-    'import/named': 0,
-    'import/default': 0,
-    'import/namespace': 0,
-    'import/no-named-as-default': 0,
-    'import/newline-after-import': 2,
-    'import/no-named-as-default-member': 0,
-    'import/no-cycle': [
-      2,
-      { maxDepth: '∞', ignoreExternal: false, allowUnsafeDynamicCyclicDependency: false },
-    ],
-  },
-  /**
-   * Unused imports rules from 'eslint-plugin-unused-imports'.
-   */
-  unusedImportsPlugin: {
-    'unused-imports/no-unused-imports': 1,
-    'unused-imports/no-unused-vars': [
-      0,
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-    ],
-  },
-  /**
-   * Sort imports/exports rules from 'eslint-plugin-perfectionist'.
-   */
-  sortImportsPlugin: () => {
-    const customGroups = {
-      mui: ['custom-mui'],
-      minimals: ['custom-minimals'],
-      private: ['custom-private'],
-      auth: ['custom-auth'],
-      hooks: ['custom-hooks'],
-      utils: ['custom-utils'],
-      types: ['custom-types'],
-      routes: ['custom-routes'],
-      sections: ['custom-sections'],
-      components: ['custom-components'],
-    };
+const commonRules = () => ({
+  ...reactHooksPlugin.configs.recommended.rules,
+  'func-names': 1,
+  'no-bitwise': 2,
+  'no-unused-vars': 0,
+  'object-shorthand': 1,
+  'no-useless-rename': 1,
+  'default-case-last': 2,
+  'consistent-return': 2,
+  'no-constant-condition': 1,
+  'default-case': [2, { commentPattern: '^no default$' }],
+  'lines-around-directive': [2, { before: 'always', after: 'always' }],
+  'arrow-body-style': ['error', 'as-needed', { requireReturnForObjectLiteral: false }],
+  // react
+  'react/jsx-key': 0,
+  'react/prop-types': 0,
+  'react/display-name': 0,
+  'react/no-children-prop': 0,
+  'react/jsx-boolean-value': 2,
+  'react/self-closing-comp': 2,
+  'react/react-in-jsx-scope': 0,
+  'react/jsx-no-useless-fragment': [1, { allowExpressions: true }],
+  'react/jsx-curly-brace-presence': [2, { props: 'never', children: 'never' }],
+  // typescript
+  '@typescript-eslint/no-shadow': 2,
+  '@typescript-eslint/no-explicit-any': 0,
+  '@typescript-eslint/no-empty-object-type': 0,
+  '@typescript-eslint/consistent-type-imports': 1,
+  '@typescript-eslint/no-unused-vars': [1, { args: 'none' }],
+});
 
-    return {
-      'perfectionist/sort-named-imports': [1, { type: 'line-length', order: 'asc' }],
-      'perfectionist/sort-named-exports': [1, { type: 'line-length', order: 'asc' }],
-      'perfectionist/sort-exports': [
-        1,
-        {
-          order: 'asc',
-          type: 'line-length',
-          groupKind: 'values-first',
-        },
-      ],
-      'perfectionist/sort-imports': [
-        2,
-        {
-          order: 'asc',
-          ignoreCase: true,
-          type: 'line-length',
-          environment: 'node',
-          maxLineLength: undefined,
-          newlinesBetween: 'always',
-          internalPattern: ['src/**'],
-          groups: [
-            'style',
-            'side-effect',
-            'type',
-            ['builtin', 'external'],
-            customGroups.mui,
-            customGroups.minimals,
-            customGroups.private,
-            customGroups.routes,
-            customGroups.hooks,
-            customGroups.utils,
-            'internal',
-            customGroups.components,
-            customGroups.sections,
-            customGroups.auth,
-            customGroups.types,
-            ['parent', 'sibling', 'index'],
-            ['parent-type', 'sibling-type', 'index-type'],
-            'object',
-            'unknown',
-          ],
-          customGroups: {
-            value: {
-              [customGroups.mui]: '@mui/**',
-              [customGroups.minimals]: 'minimal-shared/**',
-              [customGroups.private]: 'internal-ui/**',
-              [customGroups.auth]: 'src/auth/**',
-              [customGroups.hooks]: 'src/hooks/**',
-              [customGroups.utils]: 'src/utils/**',
-              [customGroups.types]: 'src/types/**',
-              [customGroups.routes]: 'src/routes/**',
-              [customGroups.sections]: 'src/sections/**',
-              [customGroups.components]: 'src/components/**',
-            },
+/**
+ * @rules import
+ * from 'eslint-plugin-import'.
+ */
+const importRules = () => ({
+  ...importPlugin.configs.recommended.rules,
+  'import/named': 0,
+  'import/export': 0,
+  'import/default': 0,
+  'import/namespace': 0,
+  'import/no-named-as-default': 0,
+  'import/newline-after-import': 2,
+  'import/no-named-as-default-member': 0,
+  'import/no-cycle': [
+    2,
+    { maxDepth: '∞', ignoreExternal: false, allowUnsafeDynamicCyclicDependency: false },
+  ],
+});
+
+/**
+ * @rules unused imports
+ * from 'eslint-plugin-unused-imports'.
+ */
+const unusedImportsRules = () => ({
+  'unused-imports/no-unused-imports': 1,
+  'unused-imports/no-unused-vars': [
+    0,
+    { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+  ],
+});
+
+/**
+ * @rules sort or imports/exports
+ * from 'eslint-plugin-perfectionist'.
+ */
+const sortImportsRules = () => {
+  const customGroups = {
+    mui: ['custom-mui'],
+    auth: ['custom-auth'],
+    hooks: ['custom-hooks'],
+    utils: ['custom-utils'],
+    types: ['custom-types'],
+    routes: ['custom-routes'],
+    sections: ['custom-sections'],
+    components: ['custom-components'],
+  };
+
+  return {
+    'perfectionist/sort-named-imports': [1, { type: 'line-length', order: 'asc' }],
+    'perfectionist/sort-named-exports': [1, { type: 'line-length', order: 'asc' }],
+    'perfectionist/sort-exports': [
+      1,
+      {
+        order: 'asc',
+        type: 'line-length',
+        groupKind: 'values-first',
+      },
+    ],
+    'perfectionist/sort-imports': [
+      2,
+      {
+        order: 'asc',
+        ignoreCase: true,
+        type: 'line-length',
+        environment: 'node',
+        maxLineLength: undefined,
+        newlinesBetween: 'always',
+        internalPattern: ['src/**'],
+        groups: [
+          'style',
+          'side-effect',
+          'type',
+          ['builtin', 'external'],
+          customGroups.mui,
+          customGroups.routes,
+          customGroups.hooks,
+          customGroups.utils,
+          'internal',
+          customGroups.components,
+          customGroups.sections,
+          customGroups.auth,
+          customGroups.types,
+          ['parent', 'sibling', 'index'],
+          ['parent-type', 'sibling-type', 'index-type'],
+          'object',
+          'unknown',
+        ],
+        customGroups: {
+          value: {
+            [customGroups.mui]: '@mui/**',
+            [customGroups.auth]: 'src/auth/**',
+            [customGroups.hooks]: 'src/hooks/**',
+            [customGroups.utils]: 'src/utils/**',
+            [customGroups.types]: 'src/types/**',
+            [customGroups.routes]: 'src/routes/**',
+            [customGroups.sections]: 'src/sections/**',
+            [customGroups.components]: 'src/components/**',
           },
         },
-      ],
-    };
-  },
+      },
+    ],
+  };
 };
 
 /**
@@ -160,7 +159,6 @@ export const customConfig = {
     import: importPlugin,
   },
   settings: {
-    react: { version: 'detect' },
     // https://www.npmjs.com/package/eslint-import-resolver-typescript
     ...importPlugin.configs.typescript.settings,
     'import/resolver': {
@@ -171,10 +169,10 @@ export const customConfig = {
     },
   },
   rules: {
-    ...customRules.common,
-    ...customRules.importPlugin,
-    ...customRules.unusedImportsPlugin,
-    ...customRules.sortImportsPlugin(),
+    ...commonRules(),
+    ...importRules(),
+    ...unusedImportsRules(),
+    ...sortImportsRules(),
   },
 };
 
@@ -185,35 +183,7 @@ export default [
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   },
   {
-    ignores: [
-      // build
-      '**/build/*',
-      '**/dist/*',
-      '**/public/*',
-      '**/out/*',
-      '**/.next/*',
-      '**/node_modules/*',
-      // src
-      '**/setupTests.*',
-      '**/jsconfig.json',
-      '**/service-worker.*',
-      '**/reportWebVitals.*',
-      '**/serviceWorkerRegistration.*',
-      // prettier
-      '**/.prettier.*',
-      '**/prettier.config.*',
-      // next
-      '**/next.config.*',
-      // vite
-      '**/vite.config.*',
-      // tailwind
-      '**/postcss.config.*',
-      '**/tailwind.config.*',
-      // craco
-      '**/craco.config.*',
-      // eslint
-      '**/eslint.config.*',
-    ],
+    ignores: ['*', '!src/', 'eslint.config.*'],
   },
   {
     languageOptions: {

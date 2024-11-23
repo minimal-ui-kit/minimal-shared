3f11478cc83da574b360fb5ec1c68db3bb71909d
minimal-ui-kit <minimal.ui.kit@gmail.com>
2024-11-23 20:37:40 +0700

    Add new transform number for input

--- packages/minimal-shared/src/utils/index.ts
+++ packages/minimal-shared/src/utils/index.ts
@@ -1,17 +1,10 @@
 export * from './url';
-
 export * from './font';
-
 export * from './color';
-
 export * from './object';
-
 export * from './uuidv4';
-
 export * from './classes';
-
 export * from './cookies';
-
 export * from './active-link';
-
 export * from './local-storage';
+export * from './transform-number';

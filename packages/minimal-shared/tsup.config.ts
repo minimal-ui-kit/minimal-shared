1b3bfcbe595b8e28988c6aabda1b05cf2f20f548
minimal-ui-kit <minimal.ui.kit@gmail.com>
2024-11-28 20:01:40 +0700

    add build cjs

--- packages/minimal-shared/tsup.config.ts
+++ packages/minimal-shared/tsup.config.ts
@@ -1,36 +1,14 @@
-import path from 'path';
-import fs from 'fs-extra';
-import glob from 'fast-glob';
 import { defineConfig } from 'tsup';
 
 // ----------------------------------------------------------------------
-
-const excludedFiles = ['!src/**/*.d.ts', '!src/**/*.test.*'];
-const copyFiles = ['src/**/index.ts'];
 
 export default defineConfig((options) => ({
   dts: true,
   clean: true,
   outDir: 'dist',
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   splitting: false,
   sourcemap: false,
   minify: !options.watch,
-  entry: ['src/**/*', ...excludedFiles],
-  onSuccess: async () => {
-    try {
-      const srcIndexTsFiles: string[] = glob.sync(copyFiles);
-
-      await Promise.all(
-        srcIndexTsFiles.map(async (file) => {
-          const dest = path.join('dist', path.relative('src', file)).replace('.ts', '.js');
-          await fs.copy(file, dest);
-        })
-      );
-
-      console.log('>> Files copied successfully!');
-    } catch (error) {
-      console.error('>> Error during onSuccess:', error);
-    }
-  },
+  entry: ['src/**/*', '!src/**/*.test.*'],
 }));

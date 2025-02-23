a706a13c59d9e127a9df62f4f5dd79b738d7e9f6
minimal-ui-kit <minimal.ui.kit@gmail.com>
2025-02-23 23:11:44 +0700

    remove

--- packages/minimal-shared-test/tests/highlight-text.ts
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-
-// ----------------------------------------------------------------------
-
-export const highlightText = {
-  val: (text: string) => `${chalk.yellowBright(text)}`,
-  fn: (text: string) => `${chalk.magenta(text)}`,
-};
